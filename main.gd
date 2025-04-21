c1124434422e47c03e8e3306f2c3df63afbfbcc0
Old-Rusty <fanxiang100@gmail.com>
2025-04-21 02:45:46 +0100

    ricky_changes
    fixed fade in transition, added new enemy, added in spawner, changed animations, reorganised folders

--- main.gd
+++ main.gd
@@ -1,12 +1,22 @@
 extends Node2D
+@onready var ani_player: AnimationPlayer = $AnimationPlayer
+@onready var spawner: Spawner = $spawner
 
-# Called when the node enters the scene tree for the first time.
+var init_spawned: bool = false
+
 func _ready() -> void:
-	get_tree().paused = true
+	ani_player.play("fade_in_trans")
 
 func _exit_tree() -> void:
 	print("Node exited tree:", name)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if get_tree().get_nodes_in_group("enemy").is_empty() && init_spawned:
+		spawner.refresh_spawn_points()
+		spawner.spawn(3)
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_in_trans":
+		print("done")
+		spawner.spawn(3)
+		init_spawned = true
---
+++ scripts/components/spawner.gd
@@ -0,0 +1,28 @@
+class_name Spawner
+extends Node2D
+@export var main: Node2D
+@export var triangle: PackedScene
+
+var init_spawn_points: Array
+var empty_spawn_points: Array
+var spawn_no: int
+
+var enemy_array: Array = [triangle]
+
+func _ready() -> void:
+	init_spawn_points = get_children()
+	empty_spawn_points = init_spawn_points.duplicate()
+
+func refresh_spawn_points() -> void:
+	empty_spawn_points = init_spawn_points.duplicate()
+
+func spawn(count: int) -> void:
+	if empty_spawn_points.is_empty():
+		return
+	for i in count:
+		var spawn:Node2D = triangle.instantiate()
+		add_child(spawn)
+		var rand_spawn: int = randi_range(0, empty_spawn_points.size()-1)
+		spawn.position = empty_spawn_points[rand_spawn].position
+		spawn.rotation = randf_range(0, 2 * PI)
+		empty_spawn_points.remove_at(rand_spawn)
